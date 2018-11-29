81f62fc973ad6ecc04c17bba9fe69c4a9bb4bc14
xank <xank@qq.com>
2018-11-29 22:52:14 +0800

    clearn

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-docker stop 17disney-forecast-service \
-; docker rm 17disney-forecast-service \
-; cd /app/forecast-service \
-&& git pull \
-&& docker build -t 17disney-forecast-service . \
-&& docker run -e TZ="Asia/Shanghai" -d -p 28201:80 --name 17disney-forecast-service \
---mount type=bind,source=/app/config/forecast-service,target=/app/config \
-17disney-forecast-service npm run docker
